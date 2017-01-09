8d198948e7c32d9358c3070859f5b204832ec9b9
Yleana Vlaun <cyvlaun4@gmail.com>
2017-01-09 13:09:11 -0600

    Practice first JAVA class exercise

--- src/MyClass.java
+++ src/MyClass.java
@@ -3,7 +3,14 @@
  */
 public class MyClass {
     public static void main(String[] args) {
-        //This is the first project that I do in JAVA
+        //declare a variable (name that can store some value) to store integer. Must have semicolon after defined value.
+        int x = 10;
+        
+
+        //print value of variable
+
+        System.out.println(x);
+
         //print out "Hello World" in the console
         System.out.println("Hello World!");
     }
