fa6aada87ec60fc7b9f1192d1f1aadc1df24828b
Yleana Vlaun <cyvlaun4@gmail.com>
2017-01-09 13:04:57 -0600

    Practice first JAVA class exercise

---
+++ src/MyClass.java
@@ -0,0 +1,10 @@
+/**
+ * Created by yleanavlaun on 1/9/17.
+ */
+public class MyClass {
+    public static void main(String[] args) {
+        //This is the first project that I do in JAVA
+        //print out "Hello World" in the console
+        System.out.println("Hello World!");
+    }
+}
