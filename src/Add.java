9ff22bca0056e7221947886b9b47d8e2a79c8743
Yleana Vlaun <cyvlaun4@gmail.com>
2017-01-17 00:43:03 -0600

    Practice

---
+++ src/Add.java
@@ -0,0 +1,14 @@
+/**
+ * Created by ${yleanavlaun} on ${01/10/2012}.
+ */
+public class Add {
+    public static void main(String[] args) {
+        //in order to store a value, you can create a variable with a datatype and store the value
+        //"int" is the datatype, "num1/num2" is the name of the variable and "6 & 3" is the value of the variable
+
+        int num1 = 6;
+        int num2 = 3;
+                
+        System.out.println(num1 + num2);
+    }
+}
