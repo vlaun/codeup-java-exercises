3f2664cdc133bfd887bd79864951e3ee15a35d7d
Yleana Vlaun <cyvlaun4@gmail.com>
2017-01-19 00:37:32 -0600

    FirstProgram: Practice

---
+++ src/FirstProgram.java
@@ -0,0 +1,8 @@
+/**
+ * Created by ${yleanavlaun} on ${01/10/2012}.
+ */
+public class FirstProgram {
+    public static void main(String[] args) {
+        System.out.println("First Java Program");
+    }
+}
