f112cccc4d51e0da22c77f12ccc4e514dc8dec56
Yleana Vlaun <cyvlaun4@gmail.com>
2017-02-01 01:30:36 -0600

    Variables Exercise

---
+++ src/VariableExercise.java
@@ -0,0 +1,21 @@
+/**
+ * Created by ${yleanavlaun} on ${01/10/2012}.
+ */
+public class VariableExercise {
+    public static void main(String[] args) {
+
+        int x = 10;
+        short my_variable = 20;
+        float my_decimal = (float)  4.5;
+        float my_double = (float) 11.56;
+        char my_char = 'A';
+        boolean is_true = false;
+
+        System.out.println(x);
+        System.out.println(my_variable);
+        System.out.println(my_decimal);
+        System.out.println(my_double);
+        System.out.println(my_char);
+        System.out.println(is_true);
+    }
+}
