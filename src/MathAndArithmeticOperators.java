0f342fedb1a162ccb1af6540bc212cbd3de067b1
Yleana Vlaun <cyvlaun4@gmail.com>
2017-02-01 02:02:57 -0600

    Math and Arithmetic Operators Practice Exercise

---
+++ src/MathAndArithmeticOperators.java
@@ -0,0 +1,22 @@
+/**
+ * Created by ${yleanavlaun} on ${01/10/2012}.
+ */
+public class MathAndArithmeticOperators {
+    public static void main(String[] args) {
+
+        int x = 70;
+        int y = 30;
+        //in this variable, you can change the operator to anything you want (+, -, /, *, %)
+        //for division if the answer will be a decimal and the variable is set to "int"then you will need
+        // to declare the variable as "double" and this will generate the accurate answer in decimal form
+        // % if there is a remainder, this is the answer that will be displayed
+        int answer = x % y;
+
+        System.out.println("Answer is = " + answer);
+
+    }
+
+
+
+
+}
