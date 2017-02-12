ce0820efe25e7be2436769c4191526371b05b98c
Yleana Vlaun <cyvlaun4@gmail.com>
2017-02-12 03:08:14 -0600

    First Java Program: Practice

---
+++ src/Application.java
@@ -0,0 +1,8 @@
+/**
+ * Created by ${yleanavlaun} on ${01/10/2012}.
+ */
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
