4d051bf45ff7be2cee947c9559c019aa0d70bb90
Yleana Vlaun <cyvlaun4@gmail.com>
2017-02-12 03:15:40 -0600

    Java Program: Practice Variables

--- src/Application.java
+++ src/Application.java
@@ -3,6 +3,35 @@
  */
 public class Application {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        // variable contains a type and a name.
+//        int myNumber;
+        // this initializes the variable, gives it a value
+//        myNumber = 88;
+
+        // the above code can be re-written as
+        int myNumber = 88;
+
+        //there are 8 different variable types
+
+        short myShort = 847;
+
+        long myLong = 9797;
+
+        double myDouble = 7.2343;
+        float myFloat = 324.3f;
+
+        char myChar = 'y';
+
+        boolean myBoolean = true;
+
+        byte myByte = 127;
+
+        System.out.println(myNumber);
+        System.out.println(myShort);
+        System.out.println(myLong);
+        System.out.println(myDouble);
+        System.out.println(myChar);
+        System.out.println(myBoolean);
+        System.out.println(myByte);
     }
 }
