8efa37e26f0aa55dfa831104f6efdf5bf0bb8007
Yleana Vlaun <cyvlaun4@gmail.com>
2017-02-12 23:20:39 -0600

    Java Program: Practice While Loops

---
+++ src/WhileLoops.java
@@ -0,0 +1,18 @@
+/**
+ * Created by ${yleanavlaun} on ${01/10/2012}.
+ */
+public class WhileLoops {
+    public static void main(String[] args) {
+
+        int value = 0;
+        boolean loop = value < 20;
+
+        // condition will determine whether loop will run
+        while(value < 11) {
+            System.out.println("Hello " + value);
+
+            value = value + 1;
+        }
+
+    }
+}
