02e2b9e21cfb1029809378e6a5de2640aee61511
Yleana Vlaun <cyvlaun4@gmail.com>
2017-02-12 23:45:23 -0600

    Java Program: Practice While Loops

---
+++ src/WhileLoop.java
@@ -0,0 +1,17 @@
+
+/**
+ * Created by ${yleanavlaun} on ${02/12/2017}.
+ */
+public class WhileLoop {
+    public static void main(String[] args) {
+            int i = 10;
+
+            while (i > -1) {
+                System.out.println(i);
+                i--;
+
+            }
+        }
+    }
+
+
